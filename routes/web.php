702086e653eb703bd196e8c6abb0816795e76514
helmibe48 <helmibe48@gmail.com>
2021-01-07 17:05:33 +0700

    FORM TAMBAH DATA

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,4 @@ Route::get('/mahasiswa', 'App\Http\Controllers\MahasiswaController@index');
 Route::get('/students', 'App\Http\Controllers\StudentsController@index');
 Route::get('/students/create', 'App\Http\Controllers\StudentsController@create');
 Route::get('/students/{student}', 'App\Http\Controllers\StudentsController@show');
+Route::post('/students', 'App\Http\Controllers\StudentsController@store');
